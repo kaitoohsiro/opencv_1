f2fdec45de2d80394b008daf3ccfe80d88b65773
kaitoohsiro <kaitooshiro7@gmail.com>
2021-07-10 15:56:40 +0900

    first commit

---
+++ opencv_test/test1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+int main() {
+    cv::Mat img = cv::imread("sample.jpeg");
+
+    cv::namedWindow("test", cv::WINDOW_AUTOSIZE);
+    cv::imshow("test", img);
+    cv::waitKey(0);
+    cv::destroyWindow("test");
+    return 0;
+}
